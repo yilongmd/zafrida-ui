f5423ed3b9bafee845a828f625a62f75d9b2b45b
yilongmd <317616660@qq.com>
2026-01-15 21:13:12 +0800

    增加一些js脚本模版, 重构模版的使用方式

--- src/main/resources/templates/android/android_hook_stringbuilder.js
+++ src/main/resources/templates/android/android_hook_stringbuilder.js
@@ -1,28 +1,3 @@
-// Android Hook org.json
-// 一般来说json都会使用gson这个库(by  小佳)
-Java.perform(function() {
-    // JSON处理
-    var jSONObject = Java.use("org.json.JSONObject");
-    jSONObject.put.overload('java.lang.String', 'java.lang.Object').implementation = function (a, b) {
-        console.log("jSONObject.put: ", a, b);
-        return this.put(a, b);
-    }
-    jSONObject.getString.implementation = function (a) {
-        console.log("jSONObject.getString: ", a);
-        var result = this.getString(a);
-        console.log("jSONObject.getString result: ", result);
-        return result;
-    }
-    JSONObject['optString'].overload('java.lang.String').implementation = function (str) {
-        if(str === "data"){
-            console.log('str', str)
-            getStackTraceString();
-        }
-        let result = this['optString'](str);
-        return result;
-    };
-});
-
 // Android Hook StringBuilder (Android 钩子 StringBuilder)
 // StringBuilder相关hook(by 小佳)
 function hook_stringbuilder() {
